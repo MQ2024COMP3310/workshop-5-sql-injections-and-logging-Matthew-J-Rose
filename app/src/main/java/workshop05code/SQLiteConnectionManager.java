b2784a9f3110c1a207d8d7777a4e3cc6b89a57c4
Matthew-J-Rose <100244570+Matthew-J-Rose@users.noreply.github.com>
2024-03-21 13:22:58 +1100

    Exercise 5.2.2 Attempt 2

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -146,8 +146,8 @@ public class SQLiteConnectionManager {
      */
     public boolean isValidWord(String guess) {
         //String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
-        String category = getCategory();
-        String sq1 = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
+        String guess = getCategory();
+        String sq1 = "SELECT count(id) as total FROM validWords WHERE word like' ';";
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
 
